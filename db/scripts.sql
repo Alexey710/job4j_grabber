1aecf0fbe8621849a405ce1fdfa85fc6f294f375
Aleksey Krylov <elsite2015@yandex.ru>
2021-02-03 08:34:15 +0400

    1.1. Job c параметрами [#438471]

---
+++ db/scripts.sql
@@ -0,0 +1,5 @@
+--CREATE DATABASE rabbit;
+CREATE SCHEMA schema1
+CREATE TABLE rabbit (created_date bigint);
+--select * from schema1.rabbit;
+--\dn;
---
+++ src/main/java/ru/job4j/quartz/AlertRabbit2.java
@@ -0,0 +1,86 @@
+package ru.job4j.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class AlertRabbit2 {
+
+    private static Connection connectDB() throws ClassNotFoundException, SQLException {
+        Connection connection = null;
+        try (FileReader reader = new FileReader(".\\rabbit.properties")) {
+            Properties properties = new Properties();
+            properties.load(reader);
+            Class.forName(properties.getProperty("driver-class-name"));
+            connection = DriverManager.getConnection(
+                    properties.getProperty("url"),
+                    properties.getProperty("username"),
+                    properties.getProperty("password"));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    private static void add(Connection cn, long date) {
+        try (PreparedStatement statement =
+                     cn.prepareStatement(
+                             "insert into schema1.rabbit(created_date) values (?) ")) {
+            statement.setLong(1, date);
+            statement.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try  {
+            Connection connection = connectDB();
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDataMap data = new JobDataMap();
+            data.put("connect", connection);
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(5)
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+            Thread.sleep(10000);
+            scheduler.shutdown();
+        } catch (Exception se) {
+            se.printStackTrace();
+        }
+    }
+
+    public static class Rabbit implements Job {
+
+        public Rabbit() {
+            System.out.println("constructor Rabbit");
+        }
+
+        @Override
+        public void execute(JobExecutionContext context) throws JobExecutionException {
+            System.out.println("Rabbit runs here ...");
+            Connection cn = (Connection) context.getJobDetail().getJobDataMap().get("connect");
+            add(cn, System.currentTimeMillis());
+        }
+    }
+}
